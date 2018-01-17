5ead9d3ee4e8bab1f4ea92e8f6865e937369b3c0
Michał Martyniak <michal.martyniak@linux.pl>
2018-01-17 13:26:16 +0100

    Movement works

--- sss.R
+++ sss.R
@@ -3,22 +3,40 @@ kBoardSize <- 10
 kTileSize <- 20
 kBoardSizeInPixels <- kBoardSize * kTileSize
 
-# Snake initialization
+# --- Snake initialization ---
 x <- c(3, 3, 3, 3)
 y <- c(2, 3, 4, 5)
-snake.body.coordinates <- cbind(x, y)
 
+# Matrix with 2 columns (x and y) where rows are snake's segments
+# cbind -> column bind. It merges/zips two vectors(columns) to make one matrix
+snake.body.coordinates <<- cbind(x, y)      
 
-# Drawing functions
+# Remembers last typed key
+# It allows to redo the last move by pressing ENTER 
+snake.last.action <<- "NO ACTION TAKEN YET" 
+snake.moves.counter <<- 0
+
+# --- Drawing functions---
 drawBox <- function(x, y, filling){
-    # pair of x and y as pixels
+    # It takes grid coordinates and converts them into axis scale (imagine it as pixels)
+    
+    # Pair of x and y for opposite square's corners
     corner.bottom_left <- c((x - 1) * kTileSize, (y - 1) * kTileSize)
     corner.upper_right <- c((x - 1) * kTileSize + kTileSize, (y - 1) * kTileSize + kTileSize)
         
-
+    # Draw rectangle with filling
     rect(xleft = corner.bottom_left[1], ybottom = corner.bottom_left[2], 
          xright = corner.upper_right[1], ytop = corner.upper_right[2],
          density = filling)
+}
+
+clearBoard <- function(){
+    # Force using new plot
+    
+    # type "n" means "dont draw anything at the beginning"
+    # ylim -> reverse axis (count from top to bottom)
+    # asp -> aspect ratio (keep square tiles)
+    plot((1:kBoardSizeInPixels), type = "n", ylim = rev(range(1:kBoardSizeInPixels)), asp = 1)
 }
 
 drawBoardGrid <- function(){
@@ -30,65 +48,97 @@ drawBoardGrid <- function(){
 }
 
 drawSnake <- function(){
+    # Draw each snake's segments by iterating through the entire snake.body.coordinates vector
+    
     for(row.index in (1:nrow(snake.body.coordinates))){
         # take one row and all columns (x and y)
         # print(snake.body.coordinates[row.index, ])
         x = snake.body.coordinates[row.index, 1]
         y = snake.body.coordinates[row.index, 2]
+        
+        # NA - Black filling
         drawBox(x, y, filling = NA)
     }
 }
 
+crawl <- function(new.head.x, new.head.y){
+    # Add new head as first row (push to the front)
+    snake.body.coordinates <<- rbind(c(new.head.x, new.head.y), snake.body.coordinates)
+    
+    # Remove old tail by removing entire last row
+    snake.body.coordinates <<- snake.body.coordinates[1:nrow(snake.body.coordinates) - 1, ]
+    
+    print(snake.body.coordinates)
+}
 
 
-
-
-
-readKeyboardInput <- function(){
-    snake.next_direction <- readline()
-    switch(snake.next_direction,
-           W={
+# --- Snake movement ---
+moveInDirection <- function(key){
+    # Temporary variables
+    head.x <- snake.body.coordinates[1, 1]
+    head.y <- snake.body.coordinates[1, 2]
+    
+    switch(key,
+           w={
                print('W')
-               head.x <- snake.body.coordinates[1, 1]
-               head.y <- snake.body.coordinates[1, 2]
-               
+               new.head.x <- head.x
                new.head.y <- head.y - 1
-               
-               # Add new head as first row
-               snake.body.coordinates <- rbind(c(head.x, new.head.y), snake.body.coordinates)
-               print(snake.body.coordinates)
-               # Remove old tail by removing entire last row
-               snake.body.coordinates <- snake.body.coordinates[1:nrow(snake.body.coordinates) - 1, ]
+               crawl(new.head.x, new.head.y)
            },
-           S={
+           s={
                print('S')
+               new.head.x <- head.x
+               new.head.y <- head.y + 1
+               crawl(new.head.x, new.head.y)
            },
-           A={
+           a={
                print('A')
+               new.head.x <- head.x - 1
+               new.head.y <- head.y
+               crawl(new.head.x, new.head.y)
            },
-           D={
+           d={
                print('D')
+               new.head.x <- head.x + 1
+               new.head.y <- head.y
+               crawl(new.head.x, new.head.y)
            },
-           Q={
+           q={
                stop("Exiting game...")
            },
            {
-               print("Wrong action! Please use W, S, A, D")
+               # What happens when none of keys listed above were eneterd...
+               # Leave empty
            })
+}
+
+readKeyboardInput <- function(){
+    # Read input from command line
+    snake.next_direction <- readline()
+    
+    # If ENTER then repeat last action
+    if(snake.next_direction == ""){
+        moveInDirection(snake.last.action)
+    }else{
+    # If any other key, then move and remember it
+        moveInDirection(snake.next_direction)
+        snake.last.action <<- snake.next_direction
+    }
+    
+    # Display moves counter to the console
+    snake.moves.counter <<- snake.moves.counter + 1
+    cat("Moves taken: ", snake.moves.counter)
 }
 
 # Game loop
 repeat{
-    # type = "n" means "dont draw antything at the beginning"
-    # ylim -> reverse axis (count from top to bottom)
-    # asp -> aspect ratio
-    plot((1:kBoardSizeInPixels), type = "n", ylim = rev(range(1:kBoardSizeInPixels)), asp = 1)
+    clearBoard()
     drawBoardGrid()
     drawSnake()
     readKeyboardInput()
     
     # loop delay
-    Sys.sleep(0.7)
+    # Sys.sleep(0.7)
 }
 
 
